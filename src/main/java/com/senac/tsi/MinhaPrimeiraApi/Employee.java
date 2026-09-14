32486b21254b456aa5d9324cd4aed5cc21211812
Denis <thedoliver_mach@hotmail.com>
2026-09-14 12:05:41 -0300

    feat: Employee record that handles both "old" and "new" clients

--- src/main/java/com/senac/tsi/MinhaPrimeiraApi/Employee.java
+++ src/main/java/com/senac/tsi/MinhaPrimeiraApi/Employee.java
@@ -8,35 +8,53 @@ import java.util.Objects;
 
 @Entity
 public class Employee {
-
-
     private @Id
-    @GeneratedValue Long id;
-    private String name;
+    @GeneratedValue long id;
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee() {
     }
 
-    public Employee( String name, String role) {
-        this.name = name;
+    public Employee( String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
-    public Long getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
 
     public String getName() {
-        return name;
+        return this.firstName + " " + this.lastName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        var parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getRole() {
@@ -51,19 +69,20 @@ public class Employee {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(getId(), employee.getId()) && Objects.equals(getName(), employee.getName()) && Objects.equals(getRole(), employee.getRole());
+        return getId() == employee.getId() && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(getRole(), employee.getRole());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName(), getRole());
+        return Objects.hash(getId(), firstName, lastName, getRole());
     }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 ", role='" + role + '\'' +
                 '}';
     }

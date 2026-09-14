f5ba36306d826eea7fc086ab3f16b3137d9783a2
Denis <thedoliver_mach@hotmail.com>
2026-09-14 12:07:32 -0300

    feat: Simplifying Link Creation

---
+++ src/main/java/com/senac/tsi/MinhaPrimeiraApi/EmployeeModelAssembler.java
@@ -0,0 +1,20 @@
+package com.senac.tsi.MinhaPrimeiraApi;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).getEmployeeById(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).getAllEmployees()).withRel("employees"));
+    }
+}
